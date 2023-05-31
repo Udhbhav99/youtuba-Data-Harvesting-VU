1e8709d90d54dbbed406acc6eef6a030fd8fb5d4
Udhbhav99 <128673044+Udhbhav99@users.noreply.github.com>
2023-06-01 03:33:00 +0530

    Delete trial and error.py

--- trial and error.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-conn = 'mongodb+srv://Udhbhav_Guvi:Vaddadi12@cluster0.t7i4vxz.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(conn)
-db = client['yt_data']
-coll = db['channel_info']
-
-docs
